9dbf9991cce1a7558be363007ba1d2094062cd66
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-06-11 17:47:27 +0100

    Now have a running model.

--- vignettes/data/MetaWards/runscript.sh
+++ vignettes/data/MetaWards/runscript.sh
@@ -4,6 +4,6 @@ mkdir -p raw_outputs
 
 export METAWARDSDATA=$HOME/Documents/covid/MetaWardsData
 
-metawards -d ncov -D demographics.json --mixer mix_pathways --mover move_pathways --input inputs/disease.dat -a ExtraSeedsLondon.dat -u lockdown_states.txt -o raw_outputs --force-overwrite-output --iterator iterate --start-date 2020/01/01 --theme simple --nsteps 10
+metawards --nproc 24 --nthreads 1 -d ncov.json -D demographics.json --mixer mix_pathways --mover move_pathways --input inputs/disease.dat -a ExtraSeedsLondon.dat -u lockdown_states.txt -o raw_outputs --force-overwrite-output --iterator iterate --start-date 2020/01/01 --theme simple --nsteps 10
 
 
