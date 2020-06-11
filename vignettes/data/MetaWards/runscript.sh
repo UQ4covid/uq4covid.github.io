c13e1979df52f56b90ca422bda271b29cd13d5e1
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-06-11 15:56:29 +0100

    Updated MetaWards model vignette to output mover/mixer inputs to scan file.

--- vignettes/data/MetaWards/runscript.sh
+++ vignettes/data/MetaWards/runscript.sh
@@ -4,6 +4,6 @@ mkdir -p raw_outputs
 
 export METAWARDSDATA=$HOME/Documents/covid/MetaWardsData
 
-metawards -d ncov -D demographics.json --mixer mix_pathways --move move_pathways --input inputs/disease.dat -a ExtraSeedsLondon.dat -i inputs/move_mix.dat -o raw_outputs --force-overwrite-output --iterator /home/tj/Documents/covid/uq4covid/model_config/metawards/iterate --start-date 2020/01/01 --theme simple
+metawards -d ncov -D demographics.json --mixer mix_pathways --mover move_pathways --input inputs/disease.dat -a ExtraSeedsLondon.dat -u lockdown_states.txt -o raw_outputs --force-overwrite-output --iterator iterate --start-date 2020/01/01 --theme simple --nsteps 10
 
 
