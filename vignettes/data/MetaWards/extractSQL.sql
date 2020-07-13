b40baeae3db459ef12edec391fa07abe70626ccd
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-07-13 11:19:14 +0100

    Amended SQL extractor to store ward and week as integer types to save space.

--- vignettes/data/MetaWards/extractSQL.sql
+++ vignettes/data/MetaWards/extractSQL.sql
@@ -1,7 +1,26 @@
 --.headers on
 .mode csv
-.import week_lookup.csv weeks
-.import ward_lookup.csv wards
+
+-- import lookups
+.import week_lookup.csv weeks0
+.import ward_lookup.csv wards0
+
+-- convert lookups to correct type
+alter table weeks0 add column weekI integer;
+update weeks0 set weekI = cast(week as integer);
+alter table weeks0 add column dayI integer;
+update weeks0 set dayI = cast(day as integer);
+create table weeks as select dayI as day, weekI as week from weeks0;
+drop table weeks0;
+
+alter table wards0 add column weekI integer;
+update wards0 set weekI = cast(week as integer);
+alter table wards0 add column wardI integer;
+update wards0 set wardI = cast(ward as integer);
+create table wards as select weekI as week, wardI as ward from wards0;
+drop table wards0;
+
+-- join compact to lookup
 create table temp as select compact.*, weeks.week from compact inner join weeks on compact.day = weeks.day;
 create table temp1 as select ward, week, sum(H) / 7.0 as Hprev, sum(C) / 7.0 as Cprev, max(DH) + max(DC) as Deaths, :id as output, :rep as replicate from temp group by ward, week;
 drop table compact;
