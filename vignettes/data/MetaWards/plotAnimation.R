cee322e37dd0705e2db6c7c37e2c8cbcc906daf5
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-07-14 12:28:28 +0100

    Amends to plotAnimation.

--- vignettes/data/MetaWards/plotAnimation.R
+++ vignettes/data/MetaWards/plotAnimation.R
@@ -1,17 +1,33 @@
-
-setwd("~/Documents/PostDoc/20200710_covid_plots")
-
-library(dplyr)
-library(dbplyr)
-library(tidyr)
-library(ggplot2)
-library(scico)
+## load libraries
+library(tidyverse)
 library(broom)
-library(readr)
 library(rgdal)
-library(DBI)
+library(rgeos)
+library(maptools)
+library(scico)
 library(gganimate)
-library(transformr)
+library(gifski)
+
+# ------------------------------------------------------------------------------
+# ----------------------------- Read arguments  --------------------------------
+# ------------------------------------------------------------------------------
+
+args <- commandArgs(TRUE)
+if(length(args) > 0) {
+    stopifnot(length(args) == 4)
+    ID <- args[1]
+    REP <- as.numeric(args[2])
+    VAR <- args[3]
+    OUTNAME <- args[4]
+} else {
+  stop("No arguments")
+}
+
+### tests
+#ID <- "Ens0000"
+#REP <- 1
+#VAR <- "Deaths"
+#OUTNAME <- "WeekDeaths"
 
 # ------------------------------------------------------------------------------
 # ----------------------------- Read shapefiles --------------------------------
@@ -19,12 +35,12 @@ library(transformr)
 
 trust <- readOGR(
   dsn = "shapefiles/WD11-TRUST/WD11-TRUST.shp", 
-  stringsAsFactors = F
+  stringsAsFactors = FALSE
 )
 
 sites <- readOGR(
   dsn = "shapefiles/WD11-TRUST-SITES/WD11-TRUST-SITES.shp", 
-  stringsAsFactors = F
+  stringsAsFactors = FALSE
 ) %>% 
   as_tibble() %>%
   rename(long = coords.x1, lat = coords.x2)
@@ -35,12 +51,7 @@ trust_df <- broom::tidy(trust, region = "trustId")
 # --------------------------------- Read SQL -----------------------------------
 # ------------------------------------------------------------------------------
 
-con <- DBI::dbConnect(RSQLite::SQLite(), 
-  "data/uberStages.db"
-)
-
-src_dbi(con)
-
+con <- DBI::dbConnect(RSQLite::SQLite(), "raw_outputs/uberStages.db")
 covid <- tbl(con, "compact")
 
 # ------------------------------------------------------------------------------
@@ -48,22 +59,20 @@ covid <- tbl(con, "compact")
 # - I have already created these indexes locally; you will also have to first --
 # ------------------------------------------------------------------------------
 
-# dbSendStatement(con, "CREATE INDEX ward_idx ON compact (ward);")
-# dbSendStatement(con, "CREATE INDEX week_idx ON compact (week);")
 # dbSendStatement(con, "CREATE INDEX output_idx ON compact (output);")
 
 # ------------------------------------------------------------------------------
 # ---------------------------- Plot model output -------------------------------
 # ------------------------------------------------------------------------------
 
-ward_lookup <- read_csv("data/ward_lookup.csv") %>%
+ward_lookup <- read_csv("Ward_Lookup.csv") %>%
   dplyr::select(WD11CD, WD11NM, LAD11CD, LAD11NM, ward = FID)
 
-to_trust <- read_csv("data/WD11ToAcuteTrustIncWalesHB.csv") %>%
+to_trust <- read_csv("WD11ToAcuteTrustIncWalesHB.csv") %>%
   as_tibble()
 
 data_tmp <- covid %>%
-  filter(output == "Ens0000", replicate == 1) %>%
+  filter(output == ID, replicate == REP) %>%
   collect() %>%
   mutate(
     ward = as.numeric(ward),
@@ -74,23 +83,23 @@ plot_tmp <- data_tmp %>%
   left_join(ward_lookup, by = "ward") %>%
   left_join(to_trust, by = "WD11CD") %>%
   dplyr::select(
-    ward, week, Hprev, Cprev, Deaths, output, trustId
+    ward, week, var = !!VAR, output, trustId
   ) %>%
   group_by(trustId, week) %>%
-  summarise(H_mean = mean(Hprev)) %>%
+  summarise(var = mean(var)) %>%
   ungroup() %>%
   right_join(trust_df, by = c("trustId" = "id"))
 
 p <- ggplot(data = plot_tmp) + 
   geom_polygon( 
-    aes(x = long, y = lat, group = group, fill = H_mean)
+    aes(x = long, y = lat, group = group, fill = var)
   ) +
   coord_fixed() +
   theme_bw() +
   xlab("Longitude") +
   ylab("Latitude") +
   scale_fill_scico(
-    "Trust Averaged\nHospital Prevalence", 
+    paste0("Trust Averaged\n", VAR), 
     direction = 1, 
     palette = "batlow"
   ) +
@@ -101,6 +110,9 @@ p <- ggplot(data = plot_tmp) +
 # Warning: this takes a while...
 # If you want a smoother transition mess around with nframes and fps and 
 # transformr and ggplot will handle it.
-a <- animate(p, nframes = 16, fps = 1) 
-anim_save("images/single_sim_batlow.gif", a)
+a <- animate(p, nframes = 16, fps = 1, renderer = gifski_renderer())
+
+## write to external files
+dir.create("images", showWarnings = FALSE)
+anim_save(paste0("images/", OUTNAME, ".gif"), a)
 
