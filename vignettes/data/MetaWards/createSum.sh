c1a794fa24889aa4be3bdb1d2441cd8828101b5d
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-08-17 14:21:53 +0100

    Amends to work on JASMIN - swapped outputs to csv

--- vignettes/data/MetaWards/createSum.sh
+++ vignettes/data/MetaWards/createSum.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 ## unzip archive
-bzip2 -dkf $1.bz2
+bzip2 -dkf raw_outputs/$1/stages.db.bz2
 
 ## run SQL script to produce summary table
-sqlite3 "$1" ".read extractSQL.sql"
+sqlite3 "raw_outputs/$1/stages.db" ".read extractSQL.sql"
+
+sqlite3 "raw_outputs/$1/stages.db" ".mode csv" ".headers on" ".output raw_outputs/$1/weeksums.csv" "select * from weeksums;" ".exit"
+
+#mv $1stages.db /gws/nopw/j04/covid19/public/$1stages.db
+mkdir -p public/raw_outputs/$1
+mv raw_outputs/$1/stages.db public/raw_outputs/$1/
+mv raw_outputs/$1/weeksums.csv public/raw_outputs/$1/
 
