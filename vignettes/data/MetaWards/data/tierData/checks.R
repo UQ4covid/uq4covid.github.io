9ccb43d360bafc88411fb4a90b7de54bf9d9a850
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-01-08 17:15:18 +0000

    Added checks on reworking wards.

--- vignettes/data/MetaWards/data/tierData/checks.R
+++ vignettes/data/MetaWards/data/tierData/checks.R
@@ -15,7 +15,7 @@ ward19 <- st_make_valid(ward19)
 ward11 <- st_make_valid(ward11)
 
 ## set paths
-pathToMetaWardsData <- "../../../../../MetaWardsData"
+pathToMetaWardsData <- "../../../../../../MetaWardsData"
 path <- paste0(pathToMetaWardsData, "/model_data/2011Data/")
 pathToNewData <- "2011to2019Data"
 
@@ -33,22 +33,37 @@ ward11 <- inner_join(ward11, Ward_Lookup, by = c("wd11cd" = "WD11CD", "wd11nm" =
 ward19 <- inner_join(ward19, WorkSize19, by = c("FID" = "X1"))
 ward11 <- inner_join(ward11, WorkSize, by = c("FID" = "X1"))
 
-## produce plots
-LADs <- c("City of London", "Exeter", "Hartlepool")
+## some general checks at the LAD level
+
+## extract matching LADs
+temp19 <- inner_join(Ward19_Lookup, WorkSize19, by = c("FID" = "X1"))
+temp11 <- inner_join(Ward_Lookup, WorkSize, by = c("FID" = "X1"))
+temp <- inner_join(temp19, temp11, by = c("LAD19NM" = "LAD11NM")) %>%
+    group_by(LAD19NM) %>%
+    summarise(WK11 = sum(X2.y), WK19 = sum(X2.x)) %>%
+    mutate(propdiff = WK11 / WK19) %>%
+    mutate(propdiff = ifelse(propdiff < 1, 1 / propdiff, propdiff)) %>%
+    arrange(-propdiff)
+
+## produce a few comparison plots
+LADs <- temp$LAD19NM[1:3]
 p <- list(NULL)
 j <- 1
 for(i in 1:length(LADs)) {
     p[[j]] <- filter(ward11, LAD11NM == LADs[i]) %>%
         ggplot() + geom_sf(aes(fill = X2)) + 
         scale_fill_viridis(
-            #limits = range(c(ward11$X2[ward11$LAD11NM == LADs[i]], ward19$X2[ward19$LAD19NM == LADs[i]]))
+            limits = range(c(ward11$X2[ward11$LAD11NM == LADs[i]], ward19$X2[ward19$LAD19NM == LADs[i]]))
         ) + ggtitle(LADs[i])
     j <- j + 1
     p[[j]] <- filter(ward19, LAD19NM == LADs[i]) %>%
         ggplot() + geom_sf(aes(fill = X2)) + 
         scale_fill_viridis(
-            #limits = range(c(ward11$X2[ward11$LAD11NM == LADs[i]], ward19$X2[ward19$LAD19NM == LADs[i]]))
+            limits = range(c(ward11$X2[ward11$LAD11NM == LADs[i]], ward19$X2[ward19$LAD19NM == LADs[i]]))
         ) + ggtitle(LADs[i])
     j <- j + 1
 }
 wrap_plots(p, ncol = 2)
+ggsave("LADcomps.pdf")
+
+
