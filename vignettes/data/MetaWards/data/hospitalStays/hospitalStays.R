ef585a2c0fcd0c9b5b7001e6635c7400bd67fba0
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-01-28 09:33:00 +0000

    Updated hospital stay code to fit Bayesian model.

--- vignettes/data/MetaWards/data/hospitalStays/hospitalStays.R
+++ vignettes/data/MetaWards/data/hospitalStays/hospitalStays.R
@@ -1,6 +1,9 @@
 ## load libraries
 library(tidyverse)
 library(fitdistrplus)
+library(nimble)
+library(coda)
+library(GGally)
 
 ## load data
 hosp <- read_csv("admissionToOutcomeLineList.csv", col_names = TRUE)
@@ -19,8 +22,8 @@ hosp <- mutate(hosp, ageCat = ifelse(ageCat == "70-79", "70+", ageCat)) %>%
 hosp %>%
     filter(finaloutcome == "Death" | finaloutcome == "Discharged") %>%
     ggplot(aes(x = time, fill = finaloutcome)) +
-        geom_density(alpha = 0.5, colour = NA) +
-        facet_wrap(~ageCat, scales = "free_y")
+    geom_density(alpha = 0.5, colour = NA) +
+    facet_wrap(~ageCat, scales = "free_y")
 hosp %>%
     filter(finaloutcome == "Death" | finaloutcome == "Discharged") %>%
     ggplot(aes(x = time)) +
@@ -53,10 +56,10 @@ hosp <- hosp %>%
 dplyr::select(hosp, ageCat, data) %>%
     unnest(cols = data) %>%
     ggplot(aes(x = time)) +
-        geom_density() +
-        facet_wrap(~ageCat, scales = "free_y") +
-        geom_line(aes(y = out), data = dplyr::select(hosp, ageCat, preds) %>%
-              unnest(cols = preds), col = "red")
+    geom_density() +
+    facet_wrap(~ageCat, scales = "free_y") +
+    geom_line(aes(y = out), data = dplyr::select(hosp, ageCat, preds) %>%
+                  unnest(cols = preds), col = "red")
 ggsave("bestFits.pdf")
 
 ## take a look at shape / scale
@@ -65,7 +68,7 @@ hosp$coefs
 ## all apart from last one are close to one scale
 ## so try to refit all using exponential
 hosp <- hosp %>%
-    mutate(coefs = map(exp, "estimate")) %>%
+    mutate(coefs = map_dbl(exp, "estimate")) %>%
     mutate(preds = map(coefs, ~{
         t <- 0:100
         out <- dexp(t, rate = .)
@@ -83,4 +86,148 @@ dplyr::select(hosp, ageCat, data) %>%
 ggsave("expFits.pdf")
 
 ## take a look at rates
-reduce(hosp$coefs, c)
+hosp$coefs
+
+## extract mid-points of age-classes
+hosp <- hosp %>%
+    mutate(ageCat = ifelse(ageCat == "70+", "70-80", ageCat)) %>%
+    separate(ageCat, c("LB", "UB"), sep = "-", remove = FALSE) %>%
+    mutate_at(vars(c("UB", "LB")), as.numeric) %>%
+    mutate(ageMid = (LB + UB) / 2) %>%
+    dplyr::select(-LB, -UB) %>%
+    mutate(TH = 1 /coefs)
+
+## plot rates against age
+ggplot(hosp, aes(x = ageMid, y = TH)) +
+    geom_point()
+ggsave("expFitsByAge.pdf")
+
+## fit simple log-LM
+hosp_lm <- hosp %>%
+    {lm(log(TH) ~ ageMid, data = .)}
+
+## add LM predictions
+hosp_fit <- tibble(ageMid = seq(min(hosp$ageMid), max(hosp$ageMid), length.out = 100))
+hosp_fit <- predict(hosp_lm, newdata = hosp_fit, interval = "confidence") %>%
+    as_tibble() %>%
+    set_names(c("mean", "LCI", "UCI")) %>%
+    cbind(hosp_fit)
+
+## fitted plot
+ggplot(hosp, aes(x = ageMid)) +
+    geom_point(aes(y = log(TH))) +
+    geom_line(aes(y = mean), data = hosp_fit) +
+    geom_ribbon(aes(ymin = LCI, ymax = UCI), data = hosp_fit, alpha = 0.5)
+ggsave("fittedLn.pdf")
+
+## summarise model fit
+summary(hosp_lm)
+cbind(coef(hosp_lm), confint(hosp_lm))
+
+## fit Bayesian hierarchical model to account for sampling variability
+code <- nimbleCode({
+    
+    ## fit exponential survival model to individual data
+    ## stratified by age
+    for (i in 1:nInd) {
+        t[i] ~ dexp(lambda[ageGrp[i]])
+    }
+    
+    ## fit log-linear model to 1 / rates
+    for(k in 1:nAge) {
+        lambda[k] <- 1.0 / TH[k]
+        mu[k] <- beta0 + beta1 * ageMid[k]
+        log(TH[k]) ~ dnorm(mu[k], sigma)
+    }
+    
+    ## priors
+    beta0 ~ dnorm(0, 5)
+    beta1 ~ T(dnorm(0, 1), 0, )
+    sigma ~ dexp(1)
+})
+
+## set up other components of model
+hospBayes <- hosp %>%
+    dplyr::select(ageCat, data, ageMid) %>%
+    unnest(data) %>%
+    mutate(ageGrp = factor(ageCat)) %>%
+    mutate(ageGrp = as.numeric(ageGrp))
+consts <- list(
+    nInd = nrow(hospBayes), 
+    nAge = length(unique(hospBayes$ageGrp)),
+    ageGrp = hospBayes$ageGrp,
+    ageMid = sort(unique(hospBayes$ageMid)),
+    maxAge = 100)
+data <- list(t = hospBayes$time)
+
+## sample initial values
+initFn <- function(ageMid) {
+    beta0 <- rnorm(1, 0, 1)
+    beta1 <- abs(rnorm(1, 0, 1))
+    sigma <- rexp(1, 1)
+    log_TH <- rnorm(beta0 + beta1 * ageMid, sigma)
+    TH <- exp(log_TH)
+    lambda <- 1 / TH
+    inits <- list(
+        beta0 = beta0,
+        beta1 = beta1,
+        sigma = sigma,
+        lambda = lambda,
+        TH = TH,
+        log_TH = log_TH
+    )
+    inits
+}
+
+## define the model, data, inits and constants
+model <- nimbleModel(code = code, constants = consts, data = data, inits = initFn(consts$ageMid))
+
+## compile the model
+cmodel <- compileNimble(model)
+
+## set monitors
+config <- configureMCMC(cmodel, monitors = c("beta0", "beta1", "lambda"), thin = 1)
+
+## check monitors and samplers
+config$printMonitors()
+config$printSamplers()
+
+## build the model
+built <- buildMCMC(config)
+cbuilt <- compileNimble(built)
+
+## run the model
+system.time(run <- runMCMC(cbuilt, 
+    niter = 20000, 
+    nburnin = 5000, 
+    nchains = 2, 
+    progressBar = TRUE, 
+    summary = TRUE, 
+    samplesAsCodaMCMC = TRUE, 
+    thin = 1))
+
+## plot traces
+plot(run$samples)
+samples <- as.matrix(run$samples)
+
+## predictions
+hosp_preds <- matrix(1, nrow = 1, ncol = 100) %>%
+    rbind(seq(min(hosp$ageMid), max(hosp$ageMid), length.out = 100))
+hosp_preds <- samples[, 1:2] %*% hosp_preds
+hosp_preds <- apply(hosp_preds, 2, function(x) {
+        tibble(mean = mean(x), LCI = quantile(x, probs = 0.025), UCI = quantile(x, probs = 0.975))
+    }) %>%
+    bind_rows() %>%
+    mutate(ageMid = seq(min(hosp$ageMid), max(hosp$ageMid), length.out = 100))
+
+## fitted plot
+ggplot(hosp, aes(x = ageMid)) +
+    geom_point(aes(y = log(TH))) +
+    geom_line(aes(y = mean), data = hosp_preds) +
+    geom_ribbon(aes(ymin = LCI, ymax = UCI), data = hosp_preds, alpha = 0.5)
+ggsave("fittedLnBayes.pdf")
+
+cor(samples[, 1:2])
+
+ggpairs(as_tibble(samples[, 1:2]), upper = list(continuous = "density"))
+ggsave("betas.pdf")
