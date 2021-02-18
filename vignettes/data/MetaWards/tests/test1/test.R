36a692a9f98090a4c0d2e70bfdee58d3d9cc4843
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-02-18 19:20:47 +0000

    Updated tests 1 and 2 to use new finalsize.

--- vignettes/data/MetaWards/tests/test1/test.R
+++ vignettes/data/MetaWards/tests/test1/test.R
@@ -30,11 +30,29 @@ contact <- read_csv("contact_matrix.csv", col_names = FALSE) %>%
     as.matrix()
 
 ## check NGM
-R0 <- NGM(R0 = NA, nu = pars["nu"], C = contact, N = N, 
+S0 <- c(N[1] - 10, N[-1])
+R0 <- NGM(R0 = NA, nu = pars["nu"], C = contact, S0 = S0, N = N, 
     nuA = pars["nuA"], gammaE = pars["gammaE"], pEA = pars["pEA"], gammaA = pars["gammaA"], 
     gammaP = pars["gammaP"], gammaI1 = pars["gammaI1"], pI1I2 = pars["pI1I2"], 
     gammaI2 = pars["gammaI2"])
+K <- R0$K
+R0 <- R0$R0
 print(paste0("R0 = ", R0))
+
+## reduce from large domain NGM to small domain NGM (Diekmann et al, 2010)
+Kind <- !apply(K, 1, function(x) all(x == 0))
+Ksmall <- matrix(0, nrow(K), sum(Kind))
+Ksmall[1:sum(Kind), 1:sum(Kind)] <- diag(sum(Kind))
+Ksmall <- t(Ksmall) %*% K %*% Ksmall
+
+## solve balance equation for final size (from Andreasen 2011)
+balance <- function(z, C) {
+    sum(abs(z - (1 - exp(-C %*% z))))
+}
+A <- t(N * t(Ksmall / N))
+finalsize <- optim(rep(0.5, length(N)), balance, C = A, control = list(maxit = 10000), method = "L-BFGS-B", lower = rep(0, length(N)), upper = rep(1, length(N)))
+stopifnot(finalsize$value <= 0.0006 | finalsize$convergence == 0)
+finalsize <- finalsize$par * N
 
 ## fit deterministic model
 source("detModel.R")
@@ -60,98 +78,97 @@ for(i in 1:8) {
         
         DBI::dbDisconnect(con)
         
-        ## reconstruct counts from incidence
-        rec[[i]][[j]] <- reconstruct(
-                    compact$Einc, compact$Pinc, compact$I1inc, compact$I2inc, compact$RIinc, 
-                    compact$DIinc, compact$Ainc, compact$RAinc,
-                    compact$Hinc, compact$RHinc, compact$DHinc
-                ) %>%
-                magrittr::set_colnames(c(
-                    "Einc", "E", "Pinc", "P", "I1inc", "I1", "I2inc", "I2", 
-                    "RI", "DI",
-                    "Ainc", "A", "RA",
-                    "Hinc", "H", "RH", "DH"
-                )) %>%
-                as_tibble()
-        rec[[i]][[j]]$day <- compact$day
+        stopifnot((i == 1 & nrow(compact) > 0) | (i != 0 & nrow(compact) == 0))
+        
+        if(nrow(compact) > 0) {
+            ## reconstruct counts from incidence
+            rec[[i]][[j]] <- reconstruct(
+                        compact$Einc, compact$Pinc, compact$I1inc, compact$I2inc, compact$RIinc, 
+                        compact$DIinc, compact$Ainc, compact$RAinc,
+                        compact$Hinc, compact$RHinc, compact$DHinc
+                    ) %>%
+                    magrittr::set_colnames(c(
+                        "Einc", "E", "Pinc", "P", "I1inc", "I1", "I2inc", "I2", 
+                        "RI", "DI",
+                        "Ainc", "A", "RA",
+                        "Hinc", "H", "RH", "DH"
+                    )) %>%
+                    as_tibble()
+            rec[[i]][[j]]$day <- compact$day
+        }
     }
     
-    ## collapse to mean and CIs
-    rec[[i]] <- bind_rows(rec[[i]], .id = "rep") %>%
-        select(-ends_with("inc")) %>%
-        complete(rep, day = 1:max(.$day)) %>%
-        group_by(rep) %>%
-        mutate_at(-c(1, 2), ~ifelse(day == 1 & is.na(.), 0, .)) %>%
-        fill(names(.)) %>%
-        ungroup() %>%
-        select(-rep) %>%
-        gather(stage, count, -day) %>%
-        group_by(day, stage) %>%
-        nest() %>%
-        mutate(data = map(data, ~{
-            data.frame(
-                mn = mean(.$count), 
-                LCI = quantile(.$count, probs = 0.025), 
-                UCI = quantile(.$count, probs = 0.975)
-            )
-        })) %>%
-        unnest(cols = data) %>%
-        gather(estimate, value, -day, -stage) %>%
-        spread(stage, value) %>%
-        select(day, estimate, E, P, I1, DI = I2)
+    if(nrow(compact) > 0) {
+        ## collapse to mean and CIs
+        rec[[i]] <- bind_rows(rec[[i]], .id = "rep") %>%
+            select(-ends_with("inc")) %>%
+            complete(rep, day = 1:max(.$day)) %>%
+            group_by(rep) %>%
+            mutate_at(-c(1, 2), ~ifelse(day == 1 & is.na(.), 0, .)) %>%
+            fill(names(.)) %>%
+            ungroup() %>%
+            select(-rep) %>%
+            gather(stage, count, -day) %>%
+            group_by(day, stage) %>%
+            nest() %>%
+            mutate(data = map(data, ~{
+                data.frame(
+                    mn = mean(.$count), 
+                    LCI = quantile(.$count, probs = 0.025), 
+                    UCI = quantile(.$count, probs = 0.975)
+                )
+            })) %>%
+            unnest(cols = data) %>%
+            gather(estimate, value, -day, -stage) %>%
+            spread(stage, value) %>%
+            select(day, estimate, E, P, I1, DI = I2)
+            
+        ## extract discrete-time stochastic simulations
+        tempStoch <- select(disSims, day = t, ends_with(as.character(i))) %>%
+            set_names(gsub(paste0(i, "$"), "", colnames(.))) %>%
+            gather(stage, count, -day) %>%
+            group_by(day, stage) %>%
+            nest() %>%
+            mutate(data = map(data, ~{
+                data.frame(
+                    mn = mean(.$count), 
+                    LCI = quantile(.$count, probs = 0.025), 
+                    UCI = quantile(.$count, probs = 0.975)
+                )
+            })) %>%
+            unnest(cols = data) %>%
+            gather(estimate, value, -day, -stage) %>%
+            spread(stage, value) %>%
+            select(day, estimate, E, P, I1, DI) %>%
+            ungroup()
+            
+        ## extract deterministic run
+        tempDet <- out %>%
+            select(-starts_with("S")) %>%
+            select(time, ends_with(as.character(i))) %>%
+            mutate_all(as.numeric) %>%
+            rename(day = time) %>%
+            mutate(estimate = "mn") %>%
+            set_names(gsub(paste0(as.character(i), "$"), "", colnames(.))) %>%
+            select(day, estimate, E, P, I1, DI)
         
-    ## extract discrete-time stochastic simulations
-    tempStoch <- select(disSims, day = t, ends_with(as.character(i))) %>%
-        set_names(gsub(paste0(i, "$"), "", colnames(.))) %>%
-        gather(stage, count, -day) %>%
-        group_by(day, stage) %>%
-        nest() %>%
-        mutate(data = map(data, ~{
-            data.frame(
-                mn = mean(.$count), 
-                LCI = quantile(.$count, probs = 0.025), 
-                UCI = quantile(.$count, probs = 0.975)
-            )
-        })) %>%
-        unnest(cols = data) %>%
-        gather(estimate, value, -day, -stage) %>%
-        spread(stage, value) %>%
-        select(day, estimate, E, P, I1, DI) %>%
-        ungroup()
-        
-    ## extract deterministic run
-    tempDet <- out %>%
-        select(-starts_with("S")) %>%
-        select(time, ends_with(as.character(i))) %>%
-        mutate_all(as.numeric) %>%
-        rename(day = time) %>%
-        mutate(estimate = "mn") %>%
-        set_names(gsub(paste0(as.character(i), "$"), "", colnames(.))) %>%
-        select(day, estimate, E, P, I1, DI)
-        
-    ## solve balance equation for final size (from Miller 2012)
-    balance <- function(z, R0, epsilon) {
-        abs(z - (1 - exp(-R0 * z)))
+        ## bind models
+        temp <- list(stoch =  tempStoch, det = tempDet, MW = rec[[i]]) %>%
+            bind_rows(.id = "model") %>%
+            gather(stage, count, -model, -estimate, -day) %>%
+            spread(estimate, count)
+            
+        p1 <- ggplot(temp, aes(x = day)) +
+            geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "MW"), alpha = 0.3) + 
+            geom_line(aes(y = mn, colour = stage, linetype = model)) +
+            geom_hline(yintercept = finalsize[i], linetype = "dashed") +
+            ggtitle(paste0("age", i))
+        p2 <- ggplot(filter(temp, model != "det"), aes(x = day)) +
+            geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "MW"), alpha = 0.3) +
+            geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "stoch"), alpha = 0.3) +
+            geom_line(aes(y = mn, colour = stage, linetype = model)) +
+            ggtitle(paste0("age", i))
+        print(p1 + p2)
     }
-    finalsize <- optimise(balance, interval = c(0, 1), R0 = R0)
-    finalsize <- finalsize$minimum * N[i]
-    
-    ## bind models
-    temp <- list(stoch =  tempStoch, det = tempDet, MW = rec[[i]]) %>%
-        bind_rows(.id = "model") %>%
-        gather(stage, count, -model, -estimate, -day) %>%
-        spread(estimate, count)
-        
-    p1 <- ggplot(temp, aes(x = day)) +
-        geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "MW"), alpha = 0.3) + 
-        geom_line(aes(y = mn, colour = stage, linetype = model)) +
-        geom_hline(yintercept = finalsize, linetype = "dashed") +
-        ggtitle(paste0("age", i))
-    p2 <- ggplot(filter(temp, model != "det"), aes(x = day)) +
-        geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "MW"), alpha = 0.3) +
-        geom_ribbon(aes(ymin = LCI, ymax = UCI, fill = stage), data = filter(temp, model == "stoch"), alpha = 0.3) +
-        geom_line(aes(y = mn, colour = stage, linetype = model)) +
-        ggtitle(paste0("age", i))
-    print(p1 + p2)
 }
 dev.off()
