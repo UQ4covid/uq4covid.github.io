10b474fafca4cd7dffae2deab338e16a293eb7ba
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-03-16 12:25:29 +0000

    Updated jobscript for Catalyst.

--- vignettes/data/MetaWards/jobscript.sh
+++ vignettes/data/MetaWards/jobscript.sh
@@ -15,10 +15,13 @@ mkdir -p raw_outputs
 
 #R CMD BATCH --no-restore --slave --no-save convertDesign.R 
 
-metawards -d model_code/ncov_overall.json -D model_code/demographics.json \
-          --mixer model_code/mix_pathways --mover model_code/move_pathways \
-          --input inputs/disease.dat -a ExtraSeedsLondon.dat \
-          -u inputs/user_inputs.txt -o raw_outputs --force-overwrite-output \
-          --iterator model_code/iterate --extractor model_code/ward_extractor \
-          --start-date 2020/01/01 --theme simple --nsteps 177 --no-spinner \
-          --nthreads 4
+metawards -d model_code/ncov.json -m 2011to2019Data\
+    -D model_code/demographics.json\
+    --mixer model_code/mix_pathways\
+    --mover model_code/move_pathways\
+    --input inputs/disease.dat\
+    --iterator model_code/iterator\
+    -u inputs/user_inputs.txt -o raw_outputs --force-overwrite-output \
+    --extractor model_code/ward_extractor\
+    --start-date 2020/02/22 --theme simple --nsteps 28 --no-spinner \
+    --nthreads 4
