c23eefed1f9b23746f351de174404f810068df7f
TJ McKinley <t.mckinley@exeter.ac.uk>
2021-03-22 10:27:52 +0000

    Updated postProc to work with new models.

--- vignettes/data/MetaWards/JASMIN/postProc.R
+++ vignettes/data/MetaWards/JASMIN/postProc.R
@@ -1,7 +1,5 @@
 ## R script to query database
-library(dplyr)
-library(tidyr)
-library(readr)
+library(tidyverse)
 library(magrittr)
 
 ## may also need to install RSQLite library
@@ -33,51 +31,72 @@ colnames(wards) <- c("ward", "week")
 ## print progress
 print(paste0("Currently evaluating: ", hash))
 
-## create path
-path <- paste0("../raw_outputs/", hash, "/stages.db")
+## extract files
+files <- list.files(paste0("../raw_outputs/", hash))
+files <- files[grep(glob2rx("age*.db.bz2"), files)]
 
-## unzip DB
-system(paste0("bzip2 -dkf ", path, ".bz2"))
+for(file in files) {
 
-## open database connection
-con <- DBI::dbConnect(RSQLite::SQLite(), path)
+    ## set path
+    path <- paste0("../raw_outputs/", hash, "/", file)
+    path <- gsub(".bz2", "", path)
 
-## collect database
-compact <- tbl(con, "compact") %>%
-    collect()
+    ## unzip DB
+    system(paste0("bzip2 -dkf ", path, ".bz2"))
+
+    ## open database connection
+    con <- DBI::dbConnect(RSQLite::SQLite(), path)
+
+    ## collect database
+    compact <- tbl(con, "compact") %>%
+        collect()
+        
+    ## disconnect from database
+    DBI::dbDisconnect(con)
+        
+    ## reconstruct counts from incidence
+    output <- arrange(compact, day) %>%
+        group_by(ward) %>%
+        nest() %>%
+        mutate(data = map(data, ~{
+            output <- reconstruct(
+                .$Einc, .$Pinc, .$I1inc, .$I2inc, .$RIinc, 
+                .$DIinc, .$Ainc, .$RAinc,
+                .$Hinc, .$RHinc, .$DHinc
+            ) %>%
+            magrittr::set_colnames(c(
+                "Einc", "E", "Pinc", "P", "I1inc", "I1", "I2inc", "I2", 
+                "RI", "DI",
+                "Ainc", "A", "RA",
+                "Hinc", "H", "RH", "DH"
+            )) %>%
+            as_tibble()
+            output$day <- .$day
+            output
+        })) %>%
+        unnest(cols = data) %>%
+        select(day, ward, everything())
+
+    ## join with week lookup and generate summary measures
+    output <- inner_join(output, weeks, by = "day") %>%
+        group_by(ward, week) %>%
+        summarise(Hprev = sum(H) / 7, Deaths = max(DH) + max(DI)) %>%
+        ungroup()
+        
+    ## expand to empty wards
+    output <- left_join(wards, output, by = c("ward", "week")) %>%
+        mutate_all(~replace_na(., 0))
+        
+    ## age class
+    agec <- strsplit(file, "\\.")[[1]][1]
+
+    ## write table out
+    system(paste0("mkdir -p ", filedir, "raw_outputs/", hash))
+    system(paste0("mv ", path, " ", filedir, "raw_outputs/", hash))
+    write_csv(output, paste0(filedir, "raw_outputs/", hash, "/weeksums_", agec, ".csv"))
     
-## disconnect from database
-DBI::dbDisconnect(con)
-    
-## reconstruct counts from incidence
-output <- reconstruct(
-        compact$Einc, compact$Pinc, compact$I1inc, compact$I2inc, compact$RIinc, 
-        compact$DIinc, compact$Ainc, compact$RAinc,
-        compact$Hinc, compact$RHinc, compact$DHinc
-    ) %>%
-    magrittr::set_colnames(c(
-        "Einc", "E", "Pinc", "P", "I1inc", "I1", "I2inc", "I2", 
-        "RI", "DI",
-        "Ainc", "A", "RA",
-        "Hinc", "H", "RH", "DH"
-    )) %>%
-    as_tibble()
-output$day <- compact$day
-
-## join with week lookup and generate summary measures
-output <- inner_join(output, weeks, by = "day") %>%
-    group_by(ward, week) %>%
-    summarise(Hprev = sum(H) / 7, Deaths = max(DH) + max(DI)) %>%
-    ungroup()
-    
-## expand to empty wards
-output <- left_join(wards, output, by = c("ward", "week")) %>%
-    mutate_all(~replace_na(., 0))
-
-## write table out
-system(paste0("mkdir -p ", filedir, "raw_outputs/", hash))
-system(paste0("mv ", path, " ", filedir, "raw_outputs/", hash))
-write_csv(output, paste0(filedir, "raw_outputs/", hash, "/weeksums.csv"))
+    print(paste0("Done: ", agec))
+}
  
 print(paste0(hash, ": Finished"))
 
