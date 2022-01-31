397af6569833595a2fa194f78aa8afcd1769d2ad
TJ McKinley <t.mckinley@exeter.ac.uk>
2022-01-31 22:45:30 +0000

    Fixed some bugs in truncated Skellam code, and added some more checks.

--- vignettes/data/MetaWards/tests/DATestAge/trSkellam.R
+++ vignettes/data/MetaWards/tests/DATestAge/trSkellam.R
@@ -25,6 +25,7 @@ dtskellam <- function(x, lambda1, lambda2, LB = -Inf, UB = Inf, log = FALSE) {
     
     ## more checks
     if(any(LB > UB)) stop("'LB' can't be > 'UB'")
+    if(any(round(LB) != LB & is.finite(LB)) | any(round(UB) != UB & is.finite(UB))) stop("'LB' and 'UB' must be integers")
     if(any(apply(cbind(LB, UB), 1, function(x) identical(x[1], x[2])) & !is.finite(LB))) stop("'LB' and 'UB' misspecified")
     if(any(c(lambda1, lambda2) <= 0)) stop("'lambda1' and 'lambda2' must be > 0")
     
@@ -34,17 +35,20 @@ dtskellam <- function(x, lambda1, lambda2, LB = -Inf, UB = Inf, log = FALSE) {
     ## adjust components for truncation as required
     UBind <- which(!is.finite(LB) & is.finite(UB))
     LBind <- which(is.finite(LB) & !is.finite(UB))
-    bothind <- which(!is.finite(LB) & !is.finite(UB))
+    bothind <- which(is.finite(LB) & is.finite(UB))
     if(length(UBind) > 0) {
         ldens[UBind] <- ldens[UBind] - pskellam(UB[UBind], lambda1[UBind], lambda2[UBind], lower.tail = TRUE, log.p = TRUE)
         ldens[UBind] <- ifelse(x[UBind] > UB[UBind], -Inf, ldens[UBind])
     }
     if(length(LBind) > 0) {
-        ldens[LBind] <- ldens[LBind] - pskellam(LB[LBind], lambda1[LBind], lambda2[LBind], lower.tail = FALSE, log.p = TRUE)
+        ldens[LBind] <- ldens[LBind] - pskellam(LB[LBind] - 1, lambda1[LBind], lambda2[LBind], lower.tail = FALSE, log.p = TRUE)
         ldens[LBind] <- ifelse(x[LBind] < LB[LBind], -Inf, ldens[LBind])
     }
     if(length(bothind) > 0) {
-        ldens[bothind] <- ldens[bothind] - log(diff(pskellam(c(LB[bothind], UB[bothind]), lambda1[bothind], lambda2[bothind], lower.tail = TRUE, log.p = FALSE)))
+        LBnorm <- pskellam(LB[bothind] - 1, lambda1[bothind], lambda2[bothind], lower.tail = TRUE, log.p = TRUE)
+        UBnorm <- pskellam(UB[bothind], lambda1[bothind], lambda2[bothind], lower.tail = TRUE, log.p = TRUE)
+        norm <- UBnorm + log(1 - exp(LBnorm - UBnorm))
+        ldens[bothind] <- ldens[bothind] - norm
         ldens[bothind] <- ifelse(x[bothind] < LB[bothind] | x[bothind] > UB[bothind], -Inf, ldens[bothind])
     }
     if(log) {
@@ -60,6 +64,8 @@ rtskellam <- function(n, lambda1, lambda2, LB = -Inf, UB = Inf, ntries = 1000) {
     ## checks on inputs
     if(missing(n) | missing(lambda1) | missing(lambda2)) stop("'n', 'lambda1' and 'lambda2' must be specified")
     if(any(!is.numeric(c(n, lambda1, lambda2, LB, UB, ntries)))) stop("Inputs must be numeric")
+    if(length(n) != 1) stop("'n' must be length 1")
+    if(round(n) != n | n < 1) stop("'n' must be a positive integer")
     if(length(lambda1) != n & length(lambda1) > 1) stop("'lambda1' must be of length 1 or n")
     if(length(lambda2) != n & length(lambda2) > 1) stop("'lambda2' must be of length 1 or n")
     if(length(LB) != n & length(LB) > 1) stop("'LB' must be of length 1 or n")
@@ -73,6 +79,7 @@ rtskellam <- function(n, lambda1, lambda2, LB = -Inf, UB = Inf, ntries = 1000) {
     
     ## more checks
     if(any(LB > UB)) stop("'LB' can't be > 'UB'")
+    if(any(round(LB) != LB & is.finite(LB)) | any(round(UB) != UB & is.finite(UB))) stop("'LB' and 'UB' must be integers")
     if(any(apply(cbind(LB, UB), 1, function(x) identical(x[1], x[2])) & !is.finite(LB))) stop("'LB' and 'UB' misspecified")
     if(any(c(lambda1, lambda2) <= 0)) stop("'lambda1' and 'lambda2' must be > 0")
     
@@ -83,13 +90,13 @@ rtskellam <- function(n, lambda1, lambda2, LB = -Inf, UB = Inf, ntries = 1000) {
     missind <- which(x < LB | x > UB)
     k <- 1
     while(length(missind) > 0 & k < ntries) {
-        x <- rskellam(length(missind), lambda1[missind], lambda2[missind])
+        x[missind] <- rskellam(length(missind), lambda1[missind], lambda2[missind])
         missind <- which(x < LB | x > UB)
         k <- k + 1
     }
-    if(k == ntries) {
+    if(length(missind) > 0) {
         ## if brute force doesn't work, then try inverse transform sampling
-        u <- runif(length(missind), 0, 1)
+        u <- runif(n, 0, 1)
         if(any(is.finite(LB) & !is.finite(UB))) {
             stop("Need to check this")
             browser()
@@ -123,7 +130,7 @@ rtskellam <- function(n, lambda1, lambda2, LB = -Inf, UB = Inf, ntries = 1000) {
                 # x <- rev(LB:UB)[ind[1]]
             } else {
                 ## normalising constant
-                x <- sapply(missind, function(i, u, UB, LB, lambda1, lambda2) {
+                x[missind] <- sapply(missind, function(i, u, UB, LB, lambda1, lambda2) {
                     x <- dskellam(LB[i]:UB[i], lambda1[i], lambda2[i], log = TRUE)
                     maxn <- max(x)
                     norm <- maxn + log(sum(exp(x - maxn)))
